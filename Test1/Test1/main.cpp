48f864f32a12aa6e645263a7e841fbcdea8794d0
Nikolay0092 <newlife19.02.20@gmail.com>
2022-05-25 14:12:14 +0300

    Новые возможности

--- Test1/Test1/main.cpp
+++ Test1/Test1/main.cpp
@@ -17,11 +17,7 @@ BOOL exceedsDelay(UINT cur, UINT prev, UINT delay);
 
 VOID ErrorExit(LPCSTR);
 
-int player_position[3][2]{
-        {5, 0},
-        {5, 10},
-        {0, 5}
-};
+int player_position[10][2]{ 0 };
 
 int x_coordinate = 11;
 int y_coordinate = 11;
@@ -71,7 +67,7 @@ public:
         else if (rand_count == 3 && player_position[1][1] < x_coordinate - 1) {
             player_position[1][1]++;
         }
-        else if (rand_count == 4 && player_position[1][1] > 0 && player_position[0][1] < x_coordinate) {
+        else if (rand_count == 4 && player_position[1][1] > 0 && player_position[1][1] < x_coordinate) {
             player_position[1][1]--;
         }
     };
@@ -97,14 +93,190 @@ public:
             player_position[2][1]--;
         }
     };
+    void fun4() {
+        std::lock_guard<std::recursive_mutex> lk(m);
+        std::random_device random_device; // Источник энтропии.
+        std::mt19937 generator(random_device()); // Генератор случайных чисел.
+        // (Здесь берется одно инициализирующее значение, можно брать больше)
+
+        std::uniform_int_distribution<> distribution(1, 4); // Равномерное распределение [1, 4]
+
+        int rand_count = distribution(generator); // Случайное число.
+        if (rand_count == 1 && player_position[3][0] > 0 && player_position[3][0] < y_coordinate) {
+            player_position[3][0]--;
+        }
+        else if (rand_count == 2 && player_position[3][0] < y_coordinate - 1) {
+            player_position[3][0]++;
+        }
+        else if (rand_count == 3 && player_position[3][1] < x_coordinate - 1) {
+            player_position[3][1]++;
+        }
+        else if (rand_count == 4 && player_position[3][1] > 0 && player_position[3][1] < x_coordinate) {
+            player_position[3][1]--;
+        }
+    };
+    void fun5() {
+        std::lock_guard<std::recursive_mutex> lk(m);
+        std::random_device random_device; // Источник энтропии.
+        std::mt19937 generator(random_device()); // Генератор случайных чисел.
+        // (Здесь берется одно инициализирующее значение, можно брать больше)
+
+        std::uniform_int_distribution<> distribution(1, 4); // Равномерное распределение [1, 4]
+
+        int rand_count = distribution(generator); // Случайное число.
+        if (rand_count == 1 && player_position[4][0] > 0 && player_position[4][0] < y_coordinate) {
+            player_position[4][0]--;
+        }
+        else if (rand_count == 2 && player_position[4][0] < y_coordinate - 1) {
+            player_position[4][0]++;
+        }
+        else if (rand_count == 3 && player_position[4][1] < x_coordinate - 1) {
+            player_position[4][1]++;
+        }
+        else if (rand_count == 4 && player_position[4][1] > 0 && player_position[4][1] < x_coordinate) {
+            player_position[4][1]--;
+        }
+    };
+    void fun6() {
+        std::lock_guard<std::recursive_mutex> lk(m);
+        std::random_device random_device; // Источник энтропии.
+        std::mt19937 generator(random_device()); // Генератор случайных чисел.
+        // (Здесь берется одно инициализирующее значение, можно брать больше)
+
+        std::uniform_int_distribution<> distribution(1, 4); // Равномерное распределение [1, 4]
+
+        int rand_count = distribution(generator); // Случайное число.
+        if (rand_count == 1 && player_position[5][0] > 0 && player_position[5][0] < y_coordinate) {
+            player_position[5][0]--;
+        }
+        else if (rand_count == 2 && player_position[5][0] < y_coordinate - 1) {
+            player_position[5][0]++;
+        }
+        else if (rand_count == 3 && player_position[5][1] < x_coordinate - 1) {
+            player_position[5][1]++;
+        }
+        else if (rand_count == 4 && player_position[5][1] > 0 && player_position[5][1] < x_coordinate) {
+            player_position[5][1]--;
+        }
+    };
+    void fun7() {
+        std::lock_guard<std::recursive_mutex> lk(m);
+        std::random_device random_device; // Источник энтропии.
+        std::mt19937 generator(random_device()); // Генератор случайных чисел.
+        // (Здесь берется одно инициализирующее значение, можно брать больше)
+
+        std::uniform_int_distribution<> distribution(1, 4); // Равномерное распределение [1, 4]
+
+        int rand_count = distribution(generator); // Случайное число.
+        if (rand_count == 1 && player_position[6][0] > 0 && player_position[6][0] < y_coordinate) {
+            player_position[6][0]--;
+        }
+        else if (rand_count == 2 && player_position[6][0] < y_coordinate - 1) {
+            player_position[6][0]++;
+        }
+        else if (rand_count == 3 && player_position[6][1] < x_coordinate - 1) {
+            player_position[6][1]++;
+        }
+        else if (rand_count == 4 && player_position[6][1] > 0 && player_position[6][1] < x_coordinate) {
+            player_position[6][1]--;
+        }
+    };
+    void fun8() {
+        std::lock_guard<std::recursive_mutex> lk(m);
+        std::random_device random_device; // Источник энтропии.
+        std::mt19937 generator(random_device()); // Генератор случайных чисел.
+        // (Здесь берется одно инициализирующее значение, можно брать больше)
+
+        std::uniform_int_distribution<> distribution(1, 4); // Равномерное распределение [1, 4]
+
+        int rand_count = distribution(generator); // Случайное число.
+        if (rand_count == 1 && player_position[7][0] > 0 && player_position[7][0] < y_coordinate) {
+            player_position[7][0]--;
+        }
+        else if (rand_count == 2 && player_position[7][0] < y_coordinate - 1) {
+            player_position[7][0]++;
+        }
+        else if (rand_count == 3 && player_position[7][1] < x_coordinate - 1) {
+            player_position[7][1]++;
+        }
+        else if (rand_count == 4 && player_position[7][1] > 0 && player_position[7][1] < x_coordinate) {
+            player_position[7][1]--;
+        }
+    };
+    void fun9() {
+        std::lock_guard<std::recursive_mutex> lk(m);
+        std::random_device random_device; // Источник энтропии.
+        std::mt19937 generator(random_device()); // Генератор случайных чисел.
+        // (Здесь берется одно инициализирующее значение, можно брать больше)
+
+        std::uniform_int_distribution<> distribution(1, 4); // Равномерное распределение [1, 4]
+
+        int rand_count = distribution(generator); // Случайное число.
+        if (rand_count == 1 && player_position[8][0] > 0 && player_position[8][0] < y_coordinate) {
+            player_position[8][0]--;
+        }
+        else if (rand_count == 2 && player_position[8][0] < y_coordinate - 1) {
+            player_position[8][0]++;
+        }
+        else if (rand_count == 3 && player_position[8][1] < x_coordinate - 1) {
+            player_position[8][1]++;
+        }
+        else if (rand_count == 4 && player_position[8][1] > 0 && player_position[8][1] < x_coordinate) {
+            player_position[8][1]--;
+        }
+    };
+    void fun10() {
+        std::lock_guard<std::recursive_mutex> lk(m);
+        std::random_device random_device; // Источник энтропии.
+        std::mt19937 generator(random_device()); // Генератор случайных чисел.
+        // (Здесь берется одно инициализирующее значение, можно брать больше)
+
+        std::uniform_int_distribution<> distribution(1, 4); // Равномерное распределение [1, 4]
+
+        int rand_count = distribution(generator); // Случайное число.
+        if (rand_count == 1 && player_position[9][0] > 0 && player_position[9][0] < y_coordinate) {
+            player_position[9][0]--;
+        }
+        else if (rand_count == 2 && player_position[9][0] < y_coordinate - 1) {
+            player_position[9][0]++;
+        }
+        else if (rand_count == 3 && player_position[9][1] < x_coordinate - 1) {
+            player_position[9][1]++;
+        }
+        else if (rand_count == 4 && player_position[9][1] > 0 && player_position[9][1] < x_coordinate) {
+            player_position[9][1]--;
+        }
+    };
 };
 
-int game() {
+int game(int Number) {
     player_position[0][0] = 5; player_position[0][1] = 0;
     player_position[1][0] = 5; player_position[1][1] = 10;
     player_position[2][0] = 0; player_position[2][1] = 5;
-    x_coordinate = 11;
-    y_coordinate = 11;
+
+    player_position[3][0] = 0; player_position[3][1] = 0;
+
+    player_position[4][0] = 0; player_position[4][1] = 15;
+    player_position[5][0] = 15; player_position[5][1] = 15;
+    player_position[6][0] = 15; player_position[6][1] = 0;
+
+    player_position[7][0] = 0; player_position[7][1] = 10;
+    player_position[8][0] = 8; player_position[8][1] = 8;
+    player_position[9][0] = 10; player_position[9][1] = 0;
+    int loss = 0;
+    int x, y;
+    if (Number < 4) {
+        x_coordinate = 11;
+        y_coordinate = 11;
+        x = 5;
+        y = 10;
+    }
+    else if (Number > 3) {
+        x_coordinate = 16;
+        y_coordinate = 16;
+        x = 8;
+        y = 15;
+    }
     HANDLE hStdin;
     HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE); // для смены цветов консоли
     const short bufferSize = 128;
@@ -117,8 +289,6 @@ int game() {
 
     int index = 0; // счётких итераций
     int eventsCount = 0; // счётчик обработанных событий
-    int x = 5;
-    int y = 10;
 
     const UINT activeDuration = 10 * MILLIS_IN_SECONDS; //длительность работы программы в милисекундах
     const UINT delay = 200;
@@ -202,20 +372,21 @@ int game() {
             SetConsoleTextAttribute(handle, FOREGROUND_INTENSITY);
             printf("|");
             for (int j = 0; j < x_coordinate; j++) {
-                if ((j == player_position[0][1] && i == player_position[0][0]) ||
-                    (j == player_position[1][1] && i == player_position[1][0]) ||
-                    (j == player_position[2][1] && i == player_position[2][0])) {
-                    SetConsoleTextAttribute(handle, FOREGROUND_GREEN);
-                    printf(" x ");
+                bool check = false;
+                for (int K = 0; K < Number; K++) {
+                    if (j == player_position[K][1] && i == player_position[K][0]) {
+                        SetConsoleTextAttribute(handle, FOREGROUND_GREEN);
+                        printf(" x ");
+                        check = true;
+                    }
                 }
-                else if (j == x && i == y) {
+                if (j == x && i == y && !check) {
                     SetConsoleTextAttribute(handle, FOREGROUND_RED);
                     printf(" x ");
                 }
-                else {
+                else if (!check) {
                     printf(" * ");
                 }
-
                 SetConsoleTextAttribute(handle, FOREGROUND_INTENSITY);
                 printf("|");
             }
@@ -226,31 +397,33 @@ int game() {
         std::thread t1(&X::fun1, &x_mutex);
         std::thread t2(&X::fun2, &x_mutex);
         std::thread t3(&X::fun3, &x_mutex);
-        t1.join();
-        t2.join();
-        t3.join();
+        std::thread t4(&X::fun4, &x_mutex);
+        std::thread t5(&X::fun5, &x_mutex);
+        std::thread t6(&X::fun6, &x_mutex);
+        std::thread t7(&X::fun7, &x_mutex);
+        std::thread t8(&X::fun8, &x_mutex);
+        std::thread t9(&X::fun9, &x_mutex); 
+        std::thread t10(&X::fun10, &x_mutex);
+        t1.join(); t2.join(); t3.join();
+        t4.join(); t5.join(); t6.join();
+        t7.join(); t8.join(); t9.join();
+        t10.join();
 
-        if (player_position[0][0] == player_position[1][0] && player_position[0][1] == player_position[1][1]) {
-            player_position[0][0] = 11;
-            player_position[0][1] = 11;
+        for (int i = 0; i < Number; i++) 
+            for (int j = 0; j < Number; j++) {
+                if (player_position[i][0] == player_position[j][0] && player_position[i][1] == player_position[j][1] && i != j) {
+                    player_position[i][0] = y_coordinate;
+                    player_position[i][1] = x_coordinate;
+                    loss++;
+                }
+            }
+        for (int i = 0; i < Number; i++) {
+            if (loss == Number - 1)
+                return 0;
+            else if (x == player_position[i][1] && y == player_position[i][0])
+                return 1;
         }
-        else if (player_position[1][0] == player_position[2][0] && player_position[1][1] == player_position[2][1]) {
-            player_position[1][0] = 11;
-            player_position[1][1] = 11;
-        }
-        else if (player_position[0][0] == player_position[2][0] && player_position[0][1] == player_position[2][1]) {
-            player_position[0][0] = 11;
-            player_position[0][1] = 11;
-        }
-        if (player_position[0][0] >= y_coordinate - 1 && player_position[1][0] >= y_coordinate - 1 && player_position[2][0] >= y_coordinate - 1)
-            return 0;
-        else if (y >= y_coordinate)
-            return 1;
-        else if (x == player_position[0][1] && y == player_position[0][0])
-            return 1;
-        else if (x == player_position[1][1] && y == player_position[1][0])
-            return 1;
-        else if (x == player_position[2][1] && y == player_position[2][0])
+        if (y >= y_coordinate)
             return 1;
     }
     return 0;
@@ -262,10 +435,15 @@ int main(VOID) {
     while (!i) {
         char start = '-';
         int G = 10;
+        int number = 3;
         std::cout << "Press 'Y' to start and press 'N' to close: ";
         std::cin >> start;
         if (start == 'Y' || start == 'y') {
-            G = game();
+            std::cout << "Enter the number of players \n (from one to ten): ";
+            std::cin >> number;
+            if (number < 1)
+                continue;
+            G = game(number);
         }
         else if (start == 'N' || start == 'n')
             return 0;
